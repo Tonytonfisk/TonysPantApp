7df9a2347606925fa5e9edf0edf2478258d25649
Tonytonfisk <epx052@edu.sollentuna.se>
2017-10-07 00:17:30 +0200

    ok

---
+++ app/src/main/java/com/example/android/panta/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.android.panta;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawerLayout = (DrawerLayout) findViewById(R.id.Drawer);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout,R.string.öppna, R.string.stäng);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+}
